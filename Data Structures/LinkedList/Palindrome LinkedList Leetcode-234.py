fce3cce5f38df5f9518ca2878e26c08a03229738
Geeta Upadhyay <80974845+geetaupadhyay@users.noreply.github.com>
2021-07-20 21:53:29 +0530

    Check whether List is a palindrome

---
+++ Data Structures/LinkedList/Palindrome LinkedList Leetcode-234.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head:
+            return False 
+        Slow=head
+        Fast=head
+        while (Fast!=None and Fast.next!=None):
+            Slow=Slow.next
+            Fast=Fast.next.next
+        Slow=self.reverse(Slow)
+        Fast=head
+        while(Slow!=None):
+            if (Slow.val!=Fast.val):
+                return False
+            Slow=Slow.next
+            Fast=Fast.next
+            
+        return True
+    def reverse(self,head):
+        Prev=None
+        Curr=head
+        while Curr:
+            Next=Curr.next
+            Curr.next=Prev
+            Prev=Curr
+            Curr=Next
+        return Prev
+        
+        
+        
